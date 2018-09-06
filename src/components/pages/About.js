eacd2ed21ad59145b0e78537931972919bbb50b2
TheBestDon <donatas_bakanas@yahoo.com>
2018-09-06 19:59:55 +0300

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        obcaecati mollitia dolor culpa perferendis quidem fuga laborum magnam, a
+        voluptatem corporis voluptates tenetur corrupti veritatis, libero natus
+        dicta adipisci autem.
+      </p>
+      <p className="text-secondary">Version 1.0</p>
+    </div>
+  );
+};
+
+export default About;
